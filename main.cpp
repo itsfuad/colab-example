a68806772973c24ffded1035a8274864e160a21d
MD.Mobashir Tajuare Partho <mdpartho.11@gmail.com>
2025-12-11 21:27:54 +0600

    first work

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 
 
 #include <iostream>
+using namespace std;
+
+
+void fun(int a,int b)
+{
+  int c=a+b;
+  cout<<c;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    
+    fun(5,6);
     return 0;
 }
+

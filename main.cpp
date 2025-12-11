8860c322371384583853a43672b3c8d2aae2e4b8
Helal-AIUB <helalaiub8009@gmail.com>
2025-12-11 21:26:59 +0600

    even

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 
 
 #include <iostream>
-
+void even(int num){
+    if(num%2==0){
+        std::cout<<"Even Number"<<std::endl;
+    }
+    else{
+        std::cout<<"Odd Number"<<std::endl;
+    }
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    even(4);
     return 0;
 }
